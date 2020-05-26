0e392c7c6f48c8a2143f3efd9545d2ffc876d8b2
cucubi <cucubi48@gmail.com>
2020-05-26 15:54:38 +0900

    Update 0526.py

--- practice/0526.py
+++ practice/0526.py
@@ -1,7 +1,6 @@
-x = 2
-if x > 3:
-    print("Hello")
-elif x == 3:
-    print("Bye")
-else:
-    print("Hi")
+def chat(name1, name2, age):
+    print("%s: Hi. How old are you?" % name1)
+    print("%s: I'm %d." % (name2, age))
+
+chat("John", "Jane", 20)
+chat("Alex", "Abby", 30)
