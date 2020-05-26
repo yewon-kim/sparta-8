570503d1dc942f56757261eeaa6ef4d12737d23f
cucubi <cucubi48@gmail.com>
2020-05-26 16:03:59 +0900

    Update 0526.py

--- practice/0526.py
+++ practice/0526.py
@@ -1,6 +1,17 @@
-def chat(name1, name2, age):
-    print("%s: Hi. How old are you?" % name1)
-    print("%s: I'm %d." % (name2, age))
+# 1. Get name and age
+# if age < 10, say "안녕"
+# else if 10 <= age < 20, say "안녕하세요"
+# else say "안녕하십니까"
 
-chat("John", "Jane", 20)
-chat("Alex", "Abby", 30)
+def sayHello(name, age):
+    if age < 10:
+        print("안녕, " + name)
+    elif age >=10 and age < 20:
+        print("안녕하세요, " + name + " 학생")
+    else:
+        print("안녕하십니까, " + name + " 씨")
+
+sayHello("도윤", 5)
+sayHello("서준", 10)
+sayHello("하준", 15)
+sayHello("은우", 20)
