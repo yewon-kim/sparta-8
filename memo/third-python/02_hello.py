18aab0348290617cde794e26087c5dac52c1d945
cucubi <cucubi48@gmail.com>
2020-05-26 14:10:48 +0900

    add orderList

---
+++ memo/third-python/02_hello.py
@@ -0,0 +1,9 @@
+import requests # requests 라이브러리 설치 필요
+
+r = requests.get('http://openapi.seoul.go.kr:8088/6d4d776b466c656533356a4b4b5872/json/RealtimeCityAir/1/99')
+rjson = r.json()
+
+gus = rjson['RealtimeCityAir']['row']
+
+for gu in gus:
+	print(gu['MSRSTE_NM'], gu['IDEX_MVL'])
