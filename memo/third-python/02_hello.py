13e80b46036f5186d6e64e2236c2798c03d1c9e6
Yewon Kim <yewonkim.dev@gmail.com>
2020-05-27 20:03:55 +0900

    Update 02_hello.py

--- memo/third-python/02_hello.py
+++ memo/third-python/02_hello.py
@@ -1,13 +1,17 @@
 import requests # requests 라이브러리 설치 필요
 
 r = requests.get('http://openapi.seoul.go.kr:8088/6d4d776b466c656533356a4b4b5872/json/RealtimeCityAir/1/99')
-
-for gu in r.json()['RealtimeCityAir']['row']:
-	time = gu["MSRDT"]
+rjson = r.json()['RealtimeCityAir']['row']
+for gu in rjson:
 	name = gu['MSRSTE_NM']
 	level = gu["IDEX_MVL"]
-	print("기준일자: ", time)
 	if level < 60:
 		print(name, level, " 좋음")
 	else:
 		print(name, level, " 나쁨")
+
+for province in rjson:
+	time = province["MSRDT"]
+	name = province["MSRSTE_NM"]
+	coLevel = province["CO"]
+	print("기준시각:", time, name, "일산화탄소 농도:", coLevel)
