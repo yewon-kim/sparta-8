4c48839bc53485b31b9d6762a903614971cbe62f
Yewon Kim <yewonkim.dev@gmail.com>
2020-05-28 23:13:19 +0900

    Create 5-5_change.py

---
+++ practice/0528_Python/5-5_change.py
@@ -0,0 +1,18 @@
+# 자료구조의 변경
+# 예시: 커피숍 메뉴
+
+# set
+menu = {"커피", "우유", "주스"}
+print(menu, type(menu))
+
+# list
+menu = list(menu)
+print(menu, type(menu))
+
+# tuple
+menu = tuple(menu)
+print(menu, type(menu))
+
+# set
+menu = set(menu)
+print(menu, type(menu))
