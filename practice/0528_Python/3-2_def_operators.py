08863d5e463752ab597b45eff6423b0b57aa3fce
Yewon Kim <yewonkim.dev@gmail.com>
2020-05-28 16:24:42 +0900

    Create 3-2_def_operators.py

---
+++ practice/0528_Python/3-2_def_operators.py
@@ -0,0 +1,11 @@
+print(abs(-5)) # |-5| == 5
+print(pow(4, 2)) # 4 ^ 2 == 16
+print(max(5, 12)) # 12
+print(min(5, 12)) # 5
+print(round(3.1)) # 3 (반올림)
+print(round(3.5)) # 4 (반올림)
+
+from math import * # Python 기본제공 math 라이브러리
+print(floor(4.99)) # 4 (내림)
+print(ceil(4.01)) # 5 (올림)
+print(sqrt(16)) # 4.0 (제곱근)
