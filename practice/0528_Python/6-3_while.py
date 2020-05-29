89163533edcab9c59641a9720d2e392f134ee1bd
Yewon Kim <yewonkim.dev@gmail.com>
2020-05-29 15:12:45 +0900

    Update 6-3_while.py

--- practice/0528_Python/6-3_while.py
+++ practice/0528_Python/6-3_while.py
@@ -14,3 +14,12 @@ while person != user:
         print("어, 그래. 끊어요.")
     else:
         print("{0}을 대야지.".format(user))
+
+right = "개강해서"
+answer = ""
+while answer != right:
+    answer = input("대학생들이 3월에 강한 이유는? ")
+    if answer == right:
+        print("당신은 훌륭한 아재입니다.")
+    else:
+        print("hint: 대학이 3월에 무엇을 하는지 생각해 보세요.")
