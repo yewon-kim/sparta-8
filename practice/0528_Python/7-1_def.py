8c268610257f077a095c0097bd739522367b7f10
Yewon Kim <yewonkim.dev@gmail.com>
2020-05-30 14:43:40 +0900

    Create 7-1_def.py

---
+++ practice/0528_Python/7-1_def.py
@@ -0,0 +1,18 @@
+def open_account():
+    print("새로운 계좌가 생성되었습니다.")
+open_account()
+
+def deposit(balance, money): # 입금
+    print("입금 완료 | 잔액: {0}원".format(balance + money))
+    return balance + money
+
+def withdraw(balance, money): # 출금
+    if balance > money: # 잔액 > 출금액
+        print("출금 완료 | 잔액: {0}원".format(balance - money))
+    else:
+        print("잔액이 모자랍니다. | 잔액: {0}원".format(balance))
+    return balance - money
+
+balance = 0 # 잔액
+balance = deposit(balance, 1000)
+balance = withdraw(balance, 500)
