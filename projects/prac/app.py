affd367310bad8b21dbe201f8f77d29d51e77b99
Yewon Kim <yewonkim.dev@gmail.com>
2020-06-03 20:23:54 +0900

    Update app.py

--- projects/prac/app.py
+++ projects/prac/app.py
@@ -15,14 +15,14 @@ def home():
 def test_post():
    title_receive = request.form['title_give']
    print(title_receive)
-   return jsonify({'result':'success', 'msg': '이 요청은 POST!'})
+   return jsonify({'result':'success', 'msg': '이 요청은 POST!', "title": title_receive})
 
 # route 같아도 method가 다르면 동일 route 가능!
 @app.route('/test', methods=['GET'])
 def test_get():
    title_receive = request.args.get('title_give')
    print(title_receive)
-   return jsonify({'result':'success', 'msg': '이 요청은 GET!'})
+   return jsonify({'result':'success', 'msg': '이 요청은 GET!', "title": title_receive})
 
 if __name__ == '__main__':  
    app.run('0.0.0.0',port=5000,debug=True)
