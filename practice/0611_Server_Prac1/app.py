ea71cd648c872b1a7feac4753bf43277f0cf2e51
Yewon Kim <yewonkim.dev@gmail.com>
2020-06-12 14:04:11 +0900

    Update Server Prac1

--- practice/0611_Server_Prac1/app.py
+++ practice/0611_Server_Prac1/app.py
@@ -13,16 +13,16 @@ db = client.dbsparta                      # 'dbsparta'라는 이름의 db를 만
 def home():
    return render_template('index.html')
 
-@app.route('/memo', methods=['GET'])
+@app.route('/api/memo', methods=['GET'])
 def listing():
     # 1. 모든 document 찾기 & _id 값은 출력에서 제외하기
-    articles = list(db.prac1.find({}, {'_id': False}))
+    articles = list(db.prac1.find({}, {'_id': 0}))
 
     # 2. articles라는 키 값으로 영화정보 내려주기
     return jsonify({'result':'success', 'articles': articles})
 
 ## API 역할을 하는 부분
-@app.route('/memo', methods=['POST'])
+@app.route('/api/memo', methods=['POST'])
 def saving():
 	# 1. 클라이언트로부터 데이터를 받기
     name_receive = request.form['name_give']
@@ -49,6 +49,24 @@ def saving():
     db.prac1.insert_one(article)
 
     return jsonify({'result': 'success'})
+
+@app.route('/api/like', methods=['POST'])
+def like_article():
+    # 1. 클라이언트가 전달한 number_give를 number_receive 변수에 넣습니다.
+    number_receive = request.form['number_give']
+
+    # 2. db에서 find_one으로 number이 number_receive와 일치하는 article을 찾습니다.
+    article = db.prac1.find_one({'number': number_receive})
+
+    # 3. article의 like 에 1을 더해준 new_like 변수를 만듭니다.
+    new_like = article['like'] + 1
+
+    # 4. db목록에서 number이 number_receive인 문서의 like 를 new_like로 변경합니다.
+    # 참고: '$set' 활용하기!
+    db.prac1.update_one({'number':number_receive}, {'$set':{'like':new_like}})
+
+    # 5. 성공하면 success 메시지를 반환합니다.
+    return jsonify({'result': 'success', 'msg':'좋아요!'})
 
 if __name__ == '__main__':
    app.run('0.0.0.0',port=5000,debug=True)
